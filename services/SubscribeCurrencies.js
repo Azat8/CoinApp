92c148233bc28612e87cb483cd2f06f8b8fa207a
Azat Mkhitaryan <Azat Mkhitaryan>
2018-06-15 16:52:47 +0400

    first commit

---
+++ services/SubscribeCurrencies.js
@@ -0,0 +1,21 @@
+class SubscribeCurrencies {
+	constructor() {
+		this.apiUrl = "https://min-api.cryptocompare.com/data/";
+		this.imageUrl = "https://cryptocompare.com";
+	}
+
+	getCoins(options) {
+		let url = this.apiUrl + options.path;
+		return fetch(url, {
+			method: 'GET',
+		})
+		.then((data) => {
+			return data.json(); 
+		})
+		.catch((error) => {
+      	console.error(error);
+    });		  	
+	}
+}
+
+export default SubscribeCurrencies;
